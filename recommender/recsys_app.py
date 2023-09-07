9b322f8de9d795100aa86ba410268cdc74adb0bd
tanbk88 <tanbk88@yahoo.com>
2023-09-07 22:58:19 +0800

    7-sep push5

--- recommender/recsys_app.py
+++ recommender/recsys_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.metrics.pairwise import cosine_similarity
 
 df = pd.read_csv('./data/museum_list_cat.csv')
