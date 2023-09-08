68ea74044330e1aa150a2084f493e338ad9d3c67
tanbk88 <tanbk88@yahoo.com>
2023-09-08 08:20:51 +0800

    app pushh

--- recommender/recsys_app.py
+++ recommender/recsys_app.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 import pickle
 
-df = pd.read_csv('data/museum_list_cat.csv')
+df = pd.read_csv('museum_list_cat.csv')
 museum = df.drop(['Type', 'Summary'], axis=1)
 museum.set_index('Name', inplace=True)
 
-museum_sim = pickle.load(open('data/museum_sim.pkl', 'rb'))
+museum_sim = pickle.load(open('museum_sim.pkl', 'rb'))
 st.title('Paris Museum Recommender')
 
 choice = st.selectbox(label='Choose a museum from the drop-down list', options=museum.index)
